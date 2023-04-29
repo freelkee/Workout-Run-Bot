83d9dcce030f6b8297470754f8f380df4384dcf0
freelkee <freelkee@ya.ru>
2023-04-29 12:04:28 +0300

    Initial commit

---
+++ src/main/java/com/telegram/freelkee/workoutrunbot/WorkoutRunBotApplication.java
@@ -0,0 +1,13 @@
+package com.telegram.freelkee.workoutrunbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutRunBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutRunBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telegram/freelkee/workoutrunbot/WorkoutRunBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegram.freelkee.workoutrunbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutRunBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
